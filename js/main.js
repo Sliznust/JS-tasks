78ac0063aa49ca574fb820f976be051f7e3fc5dc
Sliznust <64199848+Sliznust@users.noreply.github.com>
2023-01-17 10:43:27 +0300

    task commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+"USE STRICT"
+// let elem = document.getElementById('elem');
+// elem.addEventListener('click', (e)=> { 
+//   if (e.target.tagName === 'UL') {
+//     let li = document.createElement('li');
+//     li.innerHTML = 'text';
+//     elem.appendChild(li);
+//   }
+//   if (e.target.tagName === 'LI') {
+//     e.target.textContent += '!';
+//   }
+// })
+let div = document.querySelector('div');
+let ul = document.querySelector('ul')
+div.addEventListener('click', function(e) {
+	if (e.target.matches('div')) {
+		console.log('клик именно по диву');
+	}
+	if (e.target.matches('li')) {
+		e.target.textContent += '!';
+	}
+  if (e.target.matches('ul')) {
+		let li = document.createElement('li');
+    li.innerHTML = 'new li';
+    ul.appendChild(li);
+	}
+});
+
+
