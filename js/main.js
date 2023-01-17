4c863c54b1ceb3d1e8c0c3d16d2ec072600134d3
Sliznust <64199848+Sliznust@users.noreply.github.com>
2023-01-17 10:58:34 +0300

    task commit

--- js/main.js
+++ js/main.js
@@ -10,20 +10,21 @@
 //     e.target.textContent += '!';
 //   }
 // })
-let div = document.querySelector('div');
-let ul = document.querySelector('ul')
-div.addEventListener('click', function(e) {
-	if (e.target.matches('div')) {
-		console.log('клик именно по диву');
-	}
-	if (e.target.matches('li')) {
-		e.target.textContent += '!';
-	}
-  if (e.target.matches('ul')) {
-		let li = document.createElement('li');
-    li.innerHTML = 'new li';
-    ul.appendChild(li);
-	}
-});
+// let div = document.querySelector('div');
+// let ul = document.querySelector('ul')
+// div.addEventListener('click', function(e) {
+// 	if (e.target.matches('div')) {
+// 		console.log('клик именно по диву');
+// 	}
+// 	if (e.target.matches('li')) {
+// 		e.target.textContent += '!';
+// 	}
+//   if (e.target.matches('ul')) {
+// 		let li = document.createElement('li');
+//     li.innerHTML = 'new li';
+//     ul.appendChild(li);
+// 	}
+// });
 
-
+let div  = document.querySelector('div');
+console.log(typeof(div));
